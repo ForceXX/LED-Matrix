11d292483a83ef09284e31257519bdfc49a9642b
ForceXX <forcexx@yahoo.de>
2015-11-16 11:41:14 +0100

    Initial Upload

---
+++ util.h
@@ -0,0 +1,107 @@
+/*
+ * util.h
+ *
+ *  Created on: 27.10.2015
+ *      Author: forcexx
+ */
+
+
+#ifndef UTIL_H
+#define UTIL_H
+
+/*
+ * Prototypes
+ */
+
+unsigned char * charInHex(char);
+
+extern unsigned char row_address[];
+
+/*
+ * Character definitions
+ */
+
+//Uppercase Letters
+extern unsigned char char_A[];
+extern unsigned char char_B[];
+extern unsigned char char_C[];
+extern unsigned char char_D[];
+extern unsigned char char_E[];
+extern unsigned char char_F[];
+extern unsigned char char_G[];
+extern unsigned char char_H[];
+extern unsigned char char_I[];
+extern unsigned char char_J[];
+extern unsigned char char_K[];
+extern unsigned char char_L[];
+extern unsigned char char_M[];
+extern unsigned char char_N[];
+extern unsigned char char_O[];
+extern unsigned char char_P[];
+extern unsigned char char_Q[];
+extern unsigned char char_R[];
+extern unsigned char char_S[];
+extern unsigned char char_T[];
+extern unsigned char char_U[];
+extern unsigned char char_V[];
+extern unsigned char char_W[];
+extern unsigned char char_X[];
+extern unsigned char char_Y[];
+extern unsigned char char_Z[];
+
+//Lowercase Letters
+extern unsigned char char_a[];
+extern unsigned char char_b[];
+extern unsigned char char_c[];
+extern unsigned char char_d[];
+extern unsigned char char_e[];
+extern unsigned char char_f[];
+extern unsigned char char_g[];
+extern unsigned char char_h[];
+extern unsigned char char_i[];
+extern unsigned char char_j[];
+extern unsigned char char_k[];
+extern unsigned char char_l[];
+extern unsigned char char_m[];
+extern unsigned char char_n[];
+extern unsigned char char_o[];
+extern unsigned char char_p[];
+extern unsigned char char_q[];
+extern unsigned char char_r[];
+extern unsigned char char_s[];
+extern unsigned char char_t[];
+extern unsigned char char_u[];
+extern unsigned char char_v[];
+extern unsigned char char_w[];
+extern unsigned char char_x[];
+extern unsigned char char_y[];
+extern unsigned char char_z[];
+
+//Special characters
+extern unsigned char char_space[];
+extern unsigned char char_smaller_as[];
+extern unsigned char char_greater_as[];
+extern unsigned char char_equals[];
+extern unsigned char char_exclamation_mark[];
+extern unsigned char char_question_mark[];
+extern unsigned char char_quotation_mark[];
+extern unsigned char char_hash[];
+extern unsigned char char_percent[];
+extern unsigned char char_and[];
+extern unsigned char char_apostrophe[];
+extern unsigned char char_bracket_open[];
+extern unsigned char char_bracket_close[];
+extern unsigned char char_plus[];
+extern unsigned char char_minus[];
+extern unsigned char char_period[];
+extern unsigned char char_comma[];
+extern unsigned char char_slash[];
+extern unsigned char char_colon[];
+extern unsigned char char_semicolon[];
+extern unsigned char char_underline[];
+extern unsigned char char_at_sign[];
+extern unsigned char char_full[];
+extern unsigned char char_clean[];
+extern unsigned char char_smiley[];
+
+#endif // UTIL_H
